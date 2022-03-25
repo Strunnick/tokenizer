260bac17191cdd0659e7e3b6ff7700300d99283a
Alexei Shangin <98459663+Strunnick@users.noreply.github.com>
2022-03-25 12:04:09 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-﻿from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-
-setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("g_analiser", ["g_analiser.pyx"])]
-)
